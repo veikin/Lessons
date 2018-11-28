cdaa9dd9c30e5770465e805569dc3e9a8111a5ac
Sushchenko <muycc05@gmail.com>
2018-11-28 14:51:41 +0200

    add

--- metiz_erik/aliens-game/alien_invasion.py
+++ metiz_erik/aliens-game/alien_invasion.py
@@ -13,16 +13,11 @@ def run_game():
             (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
-        gf.check_events()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
