0e6cd008873b58bc00c3b6df0dbd3cda754bf06f
Sushchenko <muycc05@gmail.com>
2018-11-28 18:57:08 +0200

    add app

--- Eric_Matthes/aliens-game/alien_invasion.py
+++ Eric_Matthes/aliens-game/alien_invasion.py
@@ -3,6 +3,7 @@
 import pygame
 import game_functions as gf
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 
@@ -15,9 +16,18 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
 
+    bullets = Group()
+
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+        for bullet in bullets.copy():
+        	if bullet.rect.bottom <= 0:
+        		bullets.remove(bullet)
+        print(len(bullets))
 
 run_game()
--- Eric_Matthes/aliens-game/settings.py
+++ Eric_Matthes/aliens-game/settings.py
@@ -4,7 +4,8 @@ class Settings():
         self.screen_height = 900
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
-        self.bullet.speed_factor = 1
-        self.bullet.width = 3
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet.color = 60, 60, 60	
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
