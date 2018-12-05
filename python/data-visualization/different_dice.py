6f6528b5339cb9998cd8b33989ed813bc1acf00a
Sushchenko <muycc05@gmail.com>
2018-12-05 21:38:23 +0200

    add

--- python/data-visualization/different_dice.py
+++ python/data-visualization/different_dice.py
@@ -3,11 +3,10 @@ import pygal
 from die import Die
 
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 results = []
-
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -20,11 +19,12 @@ for value in range(2, max_result+1):
 
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.title = "Results of rolling two D6 50,000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', 
+        '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequecy of Result"
 
-hist.add('D6 + D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add('D6 + D10', frequencies)
+hist.render_to_file('dice_visual.svg')
 print(frequencies)
