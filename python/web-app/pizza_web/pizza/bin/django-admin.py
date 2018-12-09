a88c7ad8b3372acfe553056a98809c86234b80b6
Sushchenko <muycc05@gmail.com>
2018-12-09 15:57:17 +0200

    add

---
+++ python/web-app/pizza_web/pizza/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/prism/devops/Lessons/python/web-app/pizza_web/pizza/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python/web-app/pizza_web/pizzas/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+        def __str__(self):
+            return self.name[:50] + "..."
