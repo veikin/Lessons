1130aba68fac51dba40dc36b1d6f9064eb7052d0
Sushchenko <muycc05@gmail.com>
2018-12-10 15:48:04 +0200

    add

---
+++ python/web-app/meal_planer/meal_planer/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/prism/devops/Lessons/python/web-app/meal_planer/meal_planer/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python/web-app/meal_planer/meal_planers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MealPlanersConfig(AppConfig):
+    name = 'meal_planers'
