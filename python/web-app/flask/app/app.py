1b5fbfc7eff27b5651436238b496dbfed0c519d3
Sushchenko <muycc05@gmail.com>
2018-12-19 20:39:19 +0200

    add

--- python/web-app/flask/app/app.py
+++ python/web-app/flask/app/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from config import Configuration
 
+from posts.blueprint import posts
+
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+app.register_blueprint(posts, url_prefix='/blog')
