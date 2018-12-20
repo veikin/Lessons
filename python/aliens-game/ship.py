c3ac90c4eb77171fbaf076671a242e84f9782a3f
Sushchenko <muycc05@gmail.com>
2018-12-20 21:49:22 +0200

    pep8

--- python/aliens-game/ship.py
+++ python/aliens-game/ship.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Ship(Sprite):
     def __init__(self, ai_settings, screen):
         super(Ship, self).__init__()
@@ -20,11 +21,11 @@ class Ship(Sprite):
         self.moving_left = False
 
     def update(self):
-    	if self.moving_right and self.rect.right < self.screen_rect.right:
-    		self.center += self.ai_settings.ship_speed_factor
-    	if self.moving_left and self.rect.left > 0:
-    		self.center -= self.ai_settings.ship_speed_factor
-    	self.rect.centerx = self.center
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+            if self.moving_left and self.rect.left > 0:
+                self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
