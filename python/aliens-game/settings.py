7de6e0a94ba569df91b4e14a015a3d15db16b7ec
Sushchenko <muycc05@gmail.com>
2018-12-20 21:49:05 +0200

    pep8

--- python/aliens-game/settings.py
+++ python/aliens-game/settings.py
@@ -3,13 +3,13 @@ class Settings():
     def __init__(self):
         self.screen_width = 1600
         self.screen_height = 900
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
 
         self.ship_limit = 3
 
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60,60,60
+        self.bullet_color = 60, 60, 60
         self.bullet_allowed = 3
 
         self.fleet_drop_speed = 10
