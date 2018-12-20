663b9a3d357c75c0165f27d32797692c13169c9c
Sushchenko <muycc05@gmail.com>
2018-12-20 21:48:50 +0200

    pep8

--- python/aliens-game/scoreboard.py
+++ python/aliens-game/scoreboard.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from ship import Ship
 
+
 class Scoreboard():
 
     def __init__(self, ai_settings, screen, stats):
@@ -23,7 +24,8 @@ class Scoreboard():
         rounded_score = int(round(self.stats.score, -1))
 
         score_str = "{:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
+        self.score_image = self.font.render
+        (score_str, True, self.text_color, self.ai_settings.bg_color)
 
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
@@ -36,16 +38,18 @@ class Scoreboard():
         self.ships.draw(self.screen)
 
     def prep_high_score(self):
-        high_score = int(round(self.stats.high_score,-1))
+        high_score = int(round(self.stats.high_score, -1))
         high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+        self.high_score_image = self.font.render
+        (high_score_str, True, self.text_color, self.ai_settings.bg_color)
 
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
     def prep_level(self):
-        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+        self.level_image = self.font.render(str(self.stats.level),
+                True, self.text_color, self.ai_settings.bg_color)
 
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
@@ -54,7 +58,7 @@ class Scoreboard():
     def prep_ships(self):
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_settings,self.screen)
+            ship = Ship(self.ai_settings, self.screen)
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
