065ab42ea5dc6fd1e0a7d438201d6589d865e005
Sushchenko <muycc05@gmail.com>
2019-01-04 11:07:56 +0200

    add reciev

--- python/web-app/server/server-client/client.py
+++ python/web-app/server/server-client/client.py
@@ -1,17 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Импортируется модуль
 import socket
 
-# Создается сокет
-sock = socket.socket()
-# Связывется сокет с адресом и портом
-sock.connect(('localhost', 9090))
-# После подключения клиент отправляет сообщение
-sock.send('hello server!')
-# Количество байт для чтения
-data = sock.recv(1024)
-# Закрыть соединение
-sock.close()
 
-print(data)
+def Main():
+    host = '127.0.0.1'
+    port = 9090
+    s = socket.socket()     # Создается сокет
+    s.connect((host, port))     # Связывется сокет с адресом и портом
+    massage = input("-> ")
+    while massage != 'q':
+        s.send(massage.encode('utf-8'))
+        data = s.recv(1024).decode('utf-8')     # Количество байт для чтения
+        print("Recieved from server: " + data)
+        massage = input("-> ")
+    s.close()   # Закрыть соединение
+
+
+if __name__ == '__main__':
+    Main()
