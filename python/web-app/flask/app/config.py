6ef3d5a841ca050b84f76dd642d68a4cc0483849
Sushchenko <muycc05@gmail.com>
2019-01-06 18:42:15 +0200

    -

--- python/web-app/flask/app/config.py
+++
@@ -1,4 +0,0 @@
-class Configuration(object):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:YbRjkftdyf:1989@localhost/test1'
--- python/web-app/flask/app/posts/blueprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-posts = Blueprint('posts', __name__, template_folder='templates')
-
-
-@posts.route('/')
-def index():
-    return render_template('posts/index.html')
