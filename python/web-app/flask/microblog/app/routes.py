e0936b031f1c78d8530c620304d0439d739c8212
Sushchenko <muycc05@gmail.com>
2019-01-07 15:26:44 +0200

    add login

--- python/web-app/flask/microblog/app/routes.py
+++ python/web-app/flask/microblog/app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -15,9 +19,15 @@ def index():
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         },
-        {
-            'author': {'username': 'Rick'},
-            'body': 'AAAAAA!!'
-        }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+              form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
