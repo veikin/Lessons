e350c09bf6e5d6a326cbefd4d9889d5f677693f9
Sushchenko <muycc05@gmail.com>
2019-03-13 14:48:47 +0200

    add

--- javascript/django_javascript/home/static/home/script.js
+++ javascript/django_javascript/home/static/home/script.js
@@ -7,5 +7,32 @@
 // }
 
 document.getElementById('wrapper').addEventListener('click', function(event) {
-    console.log(event.target)
+    var tagName = event.target.tagName.toLowerCase()
+
+    if (tagName === 'p') {
+        event.target.style.color = 'blue'
+    }
+
+    if (event.target.classList.contains('color')) {
+        event.target.style.color = 'orange'
+    }
+})
+
+document.querySelector('#alert').addEventListener('click', function () {
+    alert('Успех!')
+})
+
+document.querySelector('#confirm').addEventListener('click', function () {
+    var decision = confirm('Вы уверены,?')
+
+    if (decision) {
+        alert('Успех!')
+    }
+})
+
+document.querySelector('button').addEventListener('click', function(event) {
+    var value = document.querySelector('input').value
+
+    localStorage.setItem('headerText', value)
+
 })
