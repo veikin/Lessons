087e9ed5d4420361fcff3d50017e8660cc146242
Sushchenko <muycc05@gmail.com>
2019-03-13 18:16:09 +0200

    add

--- javascript/django_javascript/home/views.py
+++ javascript/django_javascript/home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def first(request):
+    return render(request, 'home/first.html')
+
+
+def second(request):
+    return render(request, 'home/second.html')
