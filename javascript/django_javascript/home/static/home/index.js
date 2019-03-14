ddfa32e12df115fbda68b8ed6ccb6cd71bbe0bb1
Sushchenko <muycc05@gmail.com>
2019-03-14 09:51:04 +0200

    add

--- javascript/django_javascript/home/static/home/index.js
+++ javascript/django_javascript/home/static/home/index.js
@@ -1,7 +1,10 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 
+var score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
 
 
 function startGame() {
@@ -11,6 +14,24 @@ function startGame() {
     renderBox()
 }
 
+function handleBoxClick(event) {
+    if (event.target.dataset.box) {
+        score++
+        renderBox()
+    }
+}
+
 function renderBox() {
-    
+    $game.innerHTML = ''
+    var box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    $game.insertAdjacentElement('afterbegin', box)
 }
