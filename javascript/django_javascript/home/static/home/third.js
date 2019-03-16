7ed65df65a7be438952fca1ac5dbdc566107ddad
Sushchenko <muycc05@gmail.com>
2019-03-16 19:13:05 +0200

    add

--- javascript/django_javascript/home/static/home/third.js
+++ javascript/django_javascript/home/static/home/third.js
@@ -59,17 +59,17 @@
 //
 // console.log(ford)
 
-const form = document.querySelector('form')
-
-form.addEventListener('submit', event => {
-    event.preventDefault()
-
-    const title = form.title.value
-    const text = form.text.value
-    const description = form.description.value
-
-    saveForm({title, text, description})
-})
+// const form = document.querySelector('form')
+//
+// form.addEventListener('submit', event => {
+//     event.preventDefault()
+//
+//     const title = form.title.value
+//     const text = form.text.value
+//     const description = form.description.value
+//
+//     saveForm({title, text, description})
+// })
 
 // Spread
 // function saveForm(data) {
@@ -83,13 +83,60 @@ form.addEventListener('submit', event => {
 // }
 
 // Rest
-function saveForm(...args) {
-    const [title, text, description] = args
+// function saveForm(...args) {
+//     const [title, text, description] = args
+//
+//     const formData = {
+//         date: new Date().toLocaleDateString(),
+//         title, text, description
+//     }
+//
+//     console.log('Form data:', formData)
+// }
 
-    const formData = {
-        date: new Date().toLocaleDateString(),
-        title, text, description
-    }
 
-    console.log('Form data:', formData)
-}
+// Strings
+const createLink = ({path, name}) => `<a target="_blank" href="${path}">${name}</a>`
+
+const ul = document.querySelector('ul')
+
+const google = `<li>${createLink({path: 'https://google.com', name: 'Google'})}</li>`
+
+const habr = `<li>${createLink({path: 'https://habr.com', name: 'Habr'})}</li>`
+
+ul.insertAdjacentHTML('afterbegin', google)
+ul.insertAdjacentHTML('afterbegin', habr)
+
+
+
+//RootElement <= Box <= Instance
+// class RootElement {
+//     constructor(tagName = 'div') {
+//         this.$el = document.createElement(tagName)
+//         this.$el.style.marginBottom = '20px'
+//     }
+//
+//     hide() {
+//         this.$el.style.display = 'none'
+//     }
+// }
+//
+// class Box extends RootElement {
+//     constructor(color, size = 150, tagName) {
+//         super(tagName)
+//         this.color = color
+//         this.size = size
+//     }
+//
+//     create() {
+//         this.$el.style.background = this.color
+//         this.$el.style.width = this.$el.style.height = '${this.size}px'
+//
+//         document.querySelector('.wrapper').insertAdjacentElement('afterbegin', this.$el)
+//
+//         return this
+//     }
+// }
+//
+// const redBox = new Box('red', 100, 'div').create()
+// const blueBox = new Box('blue').create()
